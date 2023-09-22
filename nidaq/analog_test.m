daee1141ef18c0aa7e642bf93e0c6057cb85ea29
baycat <anyssa.cast@gmail.com>
2023-09-22 00:54:08 -0500

    added comments and test matrix generator

--- nidaq/analog_test.m
+++ nidaq/analog_test.m
@@ -1,5 +1,6 @@
 % analog test
 
+%list data acquisition devices
 d=daqlist;
 
 %get device info
@@ -8,11 +9,19 @@ d{1,"DeviceInfo"}
 dq = daq("ni")
 
 
-d=daq("ni")
+%establish input/output
 addoutput(d, "Dev1", "ao0", "Voltage");
+
+%write to output test
 output=0;
-write(d,output’)
+write(d,output’);
 output=2.*ones(1,10000);
-write(d,output')
+write(d,output');
 output=.5.*ones(1,10000);
-write(d,output')
+write(d,output');
+
+
+
+% preform sensor test
+% load('control_signals_test.mat','X_vtp_test'); 
+% input_test_array = X_vtp_test;
