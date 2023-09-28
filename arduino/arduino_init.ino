0a5c2700620012eacef0ded0f438c2b2fcb0e3fb
baycat <anyssa.cast@gmail.com>
2023-09-28 10:51:19 -0500

    initial code from video https://www.youtube.com/watch?v=HcdjYNwrceg

---
+++ arduino/arduino_init.ino
@@ -0,0 +1,19 @@
+unsigned char bits[2048];
+void setup() {
+    pinMode(2, OUTPUT);
+    pinMode(3, OUTPUT);
+    pinMode(4, OUTPUT);
+
+    for(int i=0; i< 2048; i++) {
+        bits[i] |= 0;
+        if(i>=0 && i<16)
+            bits[i] |= 0b00000000;
+       else    
+            bits[i] |= 0b00000001;
+
+        if(i >=2 && i <12)
+            bits[i] |= 0b00010000;
+        else    
+            bits[i] |= 0b00000000;
+    }
+}
